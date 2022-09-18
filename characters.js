711d9570479e0df28ae85f431ce157e6c7f3c347
melanieinpursuit <melaniegonzalez@pursuit.org>
2022-09-18 16:30:47 -0400

    got character names to show

--- characters.js
+++ characters.js
@@ -1,24 +1,52 @@
 
 const form = document.querySelector("form")
-/* Variable for entire page. */
+// /* Variable for entire page. */
 
 const character = document.querySelector("#characters")
-/* Variable to pick out characters from HTML. */
+// /* Variable to pick out characters from HTML. */
 
 const characterSearch = document.querySelector(".character-data")
-/* Variable for specific character search. */
+// /* Variable for specific character search. */
 
-let allCharacters = [];
+// let allCharacters = [];
 
 const origAPI = "https://thronesapi.com/api/v2/Characters"
 
+fetch('https://thronesapi.com/api/v2/Characters')
+.then(response => {
+    return response.json()
+})
+.then(data => {
+    console.log(data)
+    data.forEach(char => {
+        const firstName = char.firstName
+        const lastName = char.lastName
+        const fullName = char.fullName
+        const title = char.title
+        const family = char.family
+        const image = char.imageUrl
 
-const charData = fetch(origAPI)
-.then((res) => res.json())
-// .then (data => allCharacters = data)
-.then ((result) => allCharacters.push(result))
+        // const charImg = document.createElement('p')
+        // charImg.innerHTML = img
 
-// console.log(allCharacters)
+        const charFirstName = document.createElement('p')
+        charFirstName.innerHTML = `<strong>First Name:</strong> ${firstName}`
+        
+        const charLastName = document.createElement("p")
+        charLastName.innerHTML = `<strong>Last Name:</strong> ${lastName}`
+
+        const wholeName = document.createElement("p")
+        wholeName.innerHTML = `<strong>Full Name:</strong> ${fullName}`
+
+        const charTitle = document.createElement("p")
+        charTitle.innerHTML = `<strong>Title:</strong> ${title}`
+
+        const famName = document.createElement("p")
+        famName.innerHTML = `<strong>House Name:</strong> ${family}`
+
+        characterSearch.append(charFirstName, charLastName, wholeName, charTitle, famName)
+    })
+})
 
 /* Creating event listener for submit button. */
 form.addEventListener("submit", (event) => {
@@ -26,50 +54,4 @@ form.addEventListener("submit", (event) => {
     event.preventDefault() /* Prevents page from refreshing after clicking search. */
     
     form.reset()
-    
-    let input;
-    const search = allCharacters.find(({firstName}) => firstName === input)
-    console.log(search)
-
-    // /* Variable to pick out the value of the character entered. */
-    // const baseAPI = `https://thronesapi.com/api/v2/Characters/`
-    // /* Variable for API link. */
-    // console.log(baseAPI)
-    
-    // fetch(baseAPI) /* Fetching info from API. */
-    // .then ((res) => res.json())
-    // .then ((result) => {
-
-    //     let firstName = result.firstName.value
-    //     /* Variable to represent the first name inputted. */
-
-    //     let lastName = result.lastName.value
-    //     /* Variable to represent the last name inputted. */
-
-    //     let fullName = result.fullName.value
-    //     /* Variable to represent the full name inputted. */
-
-    //     let title = result.title.value 
-    //     /* Variable to represent the title inputted. */
-
-    //     let family = result.family.value
-    //     /* Variable to represent the family inputted. */
-
-    //     const fName = document.createElement("p")
-    //     fName.innerHTML = `<strong>First Name:</strong> ${firstName}`
-
-    //     const lName = document.createElement("p")
-    //     lName.innerHTML = `<strong>Last Name:</strong> ${lastName}`
-
-    //     const wholeName = document.createElement("p")
-    //     wholeName.innerHTML = `<strong>Full Name:</strong> ${fullName}`
-
-    //     const charTitle = document.createElement("p")
-    //     charTitle.innerHTML = `<strong>Title:</strong> ${title}`
-
-    //     const famName = document.createElement("p")
-    //     famName.innerHTML = `<strong>House Name:</strong> ${family}`
-
-    //     characterSearch.append(fName, lName, wholeName, charTitle, famName)
-    // })
 })
